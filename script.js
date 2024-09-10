8a005aab6b297d6bdf256607ad3b74209dd073ac
itspitso <147466450+itspitso@users.noreply.github.com>
2024-09-10 19:59:18 +0200

    Change function to get human choice

--- script.js
+++ script.js
@@ -21,19 +21,8 @@ function getHumanChoice() {
     // Make it easy to choose by asking them to type number
     // 0 for rock, 1 for paper, 2 for scissors
     // Show error message if user inputs anything else
-    let choice = prompt('Rock, Paper, Scissors? Type 0 for Rock, 1 for Paper or 2 for Scissors');
-    if (choicehoice == 0) {
-        return 'Rock';
-    }
-    else if (choice == 1) {
-        return 'Paper';
-    }
-    else if (choice == 2) {
-        return 'Scissors';
-    }
-    else {
-        return 'Error! Try again';
-    }
+    let choice = prompt('Rock, Paper, Scissors? Please type your answer');
+    return choice;
 }
 console.log(getHumanChoice());
 
