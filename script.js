7567b0216553e2401a35e41cece922e3b77370f6
itspitso <147466450+itspitso@users.noreply.github.com>
2024-09-10 19:50:14 +0200

    Add function to accept user choice between rock, paper or scissors

--- script.js
+++ script.js
@@ -17,6 +17,25 @@ function getComputerChoice() {
 console.log(getComputerChoice());
 
 function getHumanChoice() {
-    prompt('Rock, Paper, Scissors? Type 0 for Rock, 1 for Paper or 2 for Scissors');
+    // Ask user to choose between rock paper scissors
+    // Make it easy to choose by asking them to type number
+    // 0 for rock, 1 for paper, 2 for scissors
+    // Show error message if user inputs anything else
+    let choice = prompt('Rock, Paper, Scissors? Type 0 for Rock, 1 for Paper or 2 for Scissors');
+    if (choicehoice == 0) {
+        return 'Rock';
+    }
+    else if (choice == 1) {
+        return 'Paper';
+    }
+    else if (choice == 2) {
+        return 'Scissors';
+    }
+    else {
+        return 'Error! Try again';
+    }
 }
-getHumanChoice();
+console.log(getHumanChoice());
+
+let computerScore = 0;
+let humanScore = 0;
