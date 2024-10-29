9b99b3d14e63ac52fdf3aa9ba18b823a395edafd
itspitso <147466450+itspitso@users.noreply.github.com>
2024-10-29 11:48:27 +0200

    Add styles to buttons

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function getComputerChoice() {
     }
 }
 
+const text = document.querySelector("#text");
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
@@ -40,8 +41,6 @@ function updateScore(humanScore, computerScore) {
 function chooseRock() {
     let humanSelection = "Rock";
     let computerChoice = getComputerChoice();
-    rock.style.backgroundColor = 'red';
-    rock.style.color = "white";
     playRound(humanSelection, computerChoice);
     
 }
@@ -49,16 +48,13 @@ function chooseRock() {
 function choosePaper() {
     let humanSelection = "Paper";
     let computerChoice = getComputerChoice();
-    paper.style.backgroundColor = 'red';
-    paper.style.color = "white";
     playRound(humanSelection, computerChoice);
+
 }
 
 function chooseScissors() {
     let humanSelection = "Scissors";
     let computerChoice = getComputerChoice();
-    scissors.style.backgroundColor = 'red';
-    scissors.style.color = "white";
     playRound(humanSelection, computerChoice);
 }
 
@@ -66,42 +62,38 @@ function playRound(humanSelection, computerChoice) {
     // rock beats scissors
     // scissors beats paper
     // paper beats rock
-
-    console.log(`Your choice: ${humanSelection}`);
-    console.log(`The Computer's choice: ${computerChoice}`);
+    text.innerText = `Your choice: ${humanSelection}\nThe Computer's choice: ${computerChoice}.`;
+    //console.log(`Your choice: ${humanSelection}`);
+    //console.log(`The Computer's choice: ${computerChoice}`);
 
     if (humanSelection == 'Rock') {
         if (computerChoice == 'Rock') {
-            console.log("Rock and Rock. No points!")
+            text.innerText += " Rock and Rock. No points!"
         }
         else if (computerChoice == 'Paper') {
             computerScore++;
             updateScore(humanScore, computerScore);
-            console.log(computerScore);
-            console.log("Paper beats Rock. One point for the computer!");
+            text.innerText += " Paper beats Rock. One point for the computer!";
         }
         else if (computerChoice == 'Scissors') {
             humanScore++;
             updateScore(humanScore, computerScore);
-            console.log(humanScore);
-            console.log('Rock beats Scissors. One point for the human!');
+            text.innerText += ' Rock beats Scissors. One point for the human!';
         }
     }
     else if (humanSelection == 'Paper') {
         if (computerChoice == 'Rock') {
             humanScore++;
             updateScore(humanScore, computerScore);
-            console.log(humanScore);
-            console.log("Paper beats Rock. One point for the human!");
+            text.innerText += " Paper beats Rock. One point for the human!";
         }
         else if (computerChoice == 'Paper') {
-            console.log("Paper and Paper. No points!");
+            text.innerText += " Paper and Paper. No points!";
         }
         else if (computerChoice == 'Scissors') {
             computerScore++;
             updateScore(humanScore, computerScore);
-            console.log(computerScore);
-            console.log('Scissors beats Paper. One point for the computer!');
+            text.innerText += ' Scissors beats Paper. One point for the computer!';
         }
 
     }
@@ -109,17 +101,15 @@ function playRound(humanSelection, computerChoice) {
         if (computerChoice == 'Rock') {
             computerScore++;
             updateScore(humanScore, computerScore);
-            console.log(computerScore)
-            console.log("Rock beats Scissors. One point for the computer!");
+            text.innerText += " Rock beats Scissors. One point for the computer!";
         }
         else if (computerChoice == 'Paper') {
             humanScore++;
             updateScore(humanScore, computerScore);
-            console.log(humanScore)
-            console.log("Scissors beats Paper. One point for the human!");
+            text.innerText += " Scissors beats Paper. One point for the human!";
         }
         else if (computerChoice == 'Scissors') {
-            console.log("Scissors and Scissors. No points!");
+            text.innerText += " Scissors and Scissors. No points!";
         }
     }
     if (humanScore < 5 && computerScore < 5) {
@@ -128,12 +118,12 @@ function playRound(humanSelection, computerChoice) {
         scissors.onclick = chooseScissors;
     }
     else if (humanScore === 5) {
-        console.log(`Computer Score: ${computerScore}\nHuman Score: ${humanScore}\nYou win!`);
+        text.innerText = `Computer Score: ${computerScore}\nHuman Score: ${humanScore}\nYou win!\nWant to play again? Click Rock, Paper or Scissors to start!`;
         humanScore = 0;
         computerScore = 0;
         updateScore(humanScore, computerScore);
     } else if (computerScore === 5) {
-        console.log(`Computer Score: ${computerScore}\nHuman Score: ${humanScore}\nThe computer wins!`);
+        text.innerText = `Computer Score: ${computerScore}\nHuman Score: ${humanScore}\nThe computer wins!\nWant to play again? Click Rock, Paper or Scissors to start!`;
         humanScore = 0;
         computerScore = 0;
         updateScore(humanScore, computerScore);
